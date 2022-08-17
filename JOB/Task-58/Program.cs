49d877813f41e9501986b8f1799b5b9e7208bf55
Arkashka <sevenkapsul@mail.ru>
2022-08-17 20:58:35 +0300

    Task_58_Completed

--- JOB/Task-58/Program.cs
+++ JOB/Task-58/Program.cs
@@ -20,3 +20,56 @@
 56  8   4   24
 10  6   24  49
 */
+int[,] array = new int[5, 3];
+int[,] array2 = new int[5, 3];
+
+// Метод создающий рандомный массив
+int[,] RandomArray(int[,] array)
+{
+    System.Console.WriteLine("Создан массив с рандомными числами:");
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(10);
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+    return array;
+}
+
+array = RandomArray(array);
+array2 = RandomArray(array2);
+
+// Метод создающий новый массив 
+// из произведения матриц двух входящих массивов
+int[,] GenerationTwoMatrix(int[,] array, int[,] array2)
+{
+    System.Console.WriteLine("Произведения матриц двух входящих массивов:");
+    int[,] generationTwoMatrix = new int[array.GetLength(0),array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0) && i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) && j < array2.GetLength(1); j++)
+        {
+            generationTwoMatrix[i, j] = array[i, j] * array2[i, j];
+        }
+    }
+    return generationTwoMatrix;
+}
+
+// метод выводящий двумерный массив в терминал
+void OutPutArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+OutPutArray(GenerationTwoMatrix(array, array2));
